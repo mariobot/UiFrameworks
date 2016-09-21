f347db04f5720b49c5af31b3e04d5882558660e6
Mario Andres Botero Florez <mariobot113@hotmail.com>
2016-09-21 17:01:27 -0500

    update project

--- about.me/App_Start/WebApiConfig.cs
+++ about.me/App_Start/WebApiConfig.cs
@@ -12,22 +12,22 @@ namespace about.me
     {
         public static void Register(HttpConfiguration config)
         {
-	  // Web API configuration and services
-	  // Configure Web API to use only bearer token authentication.
-	  config.SuppressDefaultHostAuthentication();
-	  config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+            // Web API configuration and services
+            // Configure Web API to use only bearer token authentication.
+            config.SuppressDefaultHostAuthentication();
+            config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
 
-	  // Use camel case for JSON data.
-	  config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            // Use camel case for JSON data.
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
 
-	  // Web API routes
-	  config.MapHttpAttributeRoutes();
+            // Web API routes
+            config.MapHttpAttributeRoutes();
 
-	  config.Routes.MapHttpRoute(
-	      name: "DefaultApi",
-	      routeTemplate: "api/{controller}/{id}",
-	      defaults: new { id = RouteParameter.Optional }
-	  );
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
         }
     }
 }
