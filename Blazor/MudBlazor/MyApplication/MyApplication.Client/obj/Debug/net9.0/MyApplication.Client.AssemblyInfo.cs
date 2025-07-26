9e6bcc731201e7dd695408e47acbbeada66dbbe8
Mario Botero <mariobot113@hotmail.com>
2025-07-26 03:32:41 -0500

    add some www root files

--- Blazor/MudBlazor/MyApplication/MyApplication.Client/obj/Debug/net9.0/MyApplication.Client.AssemblyInfo.cs
+++ Blazor/MudBlazor/MyApplication/MyApplication.Client/obj/Debug/net9.0/MyApplication.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApplication.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02c5150e81be03773578f9e6289205025b41259e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71c5d339863b5a3624a0296249787cc0f8fc17f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApplication.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApplication.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
