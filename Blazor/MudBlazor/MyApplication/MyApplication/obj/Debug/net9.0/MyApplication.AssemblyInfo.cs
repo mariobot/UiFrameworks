851314b1b87b7352966dc2aab5d73c37fb881d6d
Mario Botero <mariobot113@hotmail.com>
2025-07-26 03:39:45 -0500

    debug files

--- Blazor/MudBlazor/MyApplication/MyApplication/obj/Debug/net9.0/MyApplication.AssemblyInfo.cs
+++ Blazor/MudBlazor/MyApplication/MyApplication/obj/Debug/net9.0/MyApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+044c268ee06cdb23f9fec6c297cb1ee1f85b41a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+caa608b590787f06d4361279be79c444b50334a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
